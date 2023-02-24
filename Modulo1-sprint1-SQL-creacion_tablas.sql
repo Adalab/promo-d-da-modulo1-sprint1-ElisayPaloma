74ec381e0325181ea8cda73b857905cad7bb58af
Elisa <ejimazcona@gmail.com>
2023-02-24 13:15:09 +0100

    SQL creacion de tablas

---
+++ Modulo1-sprint1-SQL-creacion_tablas.sql
@@ -0,0 +1,52 @@
+CREATE SCHEMA `tienda_zapatillas`;
+USE `tienda_zapatillas`;
+CREATE TABLE Zapatillas(
+	id_zapatillas INT NOT NULL AUTO_INCREMENT,
+    modelo VARCHAR(45) NOT NULL,
+    color VARCHAR(45) NOT NULL,
+    PRIMARY KEY (id_zapatillas));
+  
+  
+CREATE TABLE Clientes(
+	id_cliente INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(45) NOT NULL,
+    numero_telefono INT(9) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    direccion VARCHAR(45) NOT NULL,
+    ciudad VARCHAR(45) NOT NULL,
+    provincia VARCHAR(45) NOT NULL,
+    pais VARCHAR(45) NOT NULL,
+    codigo_postal VARCHAR(45) NOT NULL,
+    PRIMARY KEY (id_cliente));
+    
+CREATE TABLE Empleados(
+	id_empleado INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(45) NOT NULL,
+    tienda VARCHAR(45) NOT NULL,
+    salario INT,
+    fecha_incorporacion DATE NOT NULL,
+    PRIMARY KEY (id_empleado));
+    
+    
+CREATE TABLE Facturas(
+	id_factura INT NOT NULL AUTO_INCREMENT,
+    numero_factura VARCHAR(45) NOT NULL,
+    fecha DATE NOT NULL,
+    id_zapatillas INT NOT NULL,
+    id_empleado INT NOT NULL,
+    id_cliente INT NOT NULL,
+    PRIMARY KEY (id_factura),
+    CONSTRAINT `fk_factura_zapatillas`
+		FOREIGN KEY (`id_zapatillas`)
+        REFERENCES `zapatillas`(id_zapatillas), 
+	CONSTRAINT `fk_factura_empleado`
+		FOREIGN KEY (`id_empleado`)
+		REFERENCES `empleado`(id_empleado),
+    CONSTRAINT `fk_factura_cliente`
+		FOREIGN KEY (`id_cliente`)
+        REFERENCES `cliente`(id_cliente) ON DELETE CASCADE ON UPDATE CASCADE);
+        
+        
+    
+    
+    
